17ca2feb4953a285959c8c3a76028af8d445fa4e
Julien Brancourt <brancourt.julien@hotmail.fr>
2024-09-12 15:21:18 +0200

    first commit

---
+++ recipify_back/recipify_back/src/main/java/org/example/recipify_back/RecipifyBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.recipify_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipifyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipifyBackApplication.class, args);
+	}
+
+}
