c70e89997754d5fa1b09f835b720c919c7d01d51
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-09-12 17:15:03 +0200

    Correction Bdd Docker et création de table
    Dans le Docker compose -> pointé vers un schéma non déterminé perte des données
    User -> Changement de user en app_user pour les règles Postgresql
    Application property -> Correction
    
    ATTENTION on ne s'appelle pas db mais postgres-db dans le docker-compose !
    
    De plus nous somme en connexion directe donc on pointe vers :
    
    jdbc:postgresql://postgres-db:5432/mydatabase?currentSchema=recipify

--- recipify_back/recipify_back/src/main/java/org/example/recipify_back/RecipifyBackApplication.java
+++ recipify_back/recipify_back/src/main/java/org/example/recipify_back/RecipifyBackApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecipifyBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RecipifyBackApplication.class, args);
 	}
-
 }
