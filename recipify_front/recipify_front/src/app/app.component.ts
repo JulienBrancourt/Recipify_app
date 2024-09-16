b1e484b57cb8b278682c49fafc8a3da2aa46fc6a
Szczapa <gilabertadam@gmail.com>
2024-09-16 11:05:47 +0200

    update visuel et mise en page générale

--- recipify_front/recipify_front/src/app/app.component.ts
+++ recipify_front/recipify_front/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipify_front';
+
+  // On vas placer un observable sur le statut de connexion de l'utilisateur pour masquer la barre de navigation si il n'est pas connecté
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
