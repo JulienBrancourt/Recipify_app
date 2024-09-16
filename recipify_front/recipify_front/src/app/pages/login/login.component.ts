00bb25f95173df61d57c4b8c9925a2c8e43fa946
LucieRg <129874296+LucieRg@users.noreply.github.com>
2024-09-16 12:06:31 +0200

    AuthService + login

--- recipify_front/recipify_front/src/app/pages/login/login.component.ts
+++ recipify_front/recipify_front/src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {AuthService} from "../../service/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -17,9 +19,35 @@ export class LoginComponent {
     password: new FormControl(''),
   });
 
-  constructor() {}
+  errorMessage: string | null = null;
+  isSubmitting: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
 
   handleSubmit() {
-    console.log("vous etes connecté");
+    if (this.form.valid) {
+      const email = this.form.get('email')?.value;
+      const password = this.form.get('password')?.value;
+
+      this.isSubmitting = true;
+      this.authService.login(email!, password!)
+        .subscribe({
+          next: (response: string) => {
+            console.log('Connexion réussie', response);
+
+            this.router.navigate(['/dashboard']);
+          },
+          error: (err: string) => {
+            console.error('Erreur de connexion', err);
+            this.errorMessage = 'Échec de la connexion. Veuillez vérifier vos identifiants.';
+          },
+          complete: () => {
+            this.isSubmitting = false;
+          }
+        });
+    } else {
+      this.errorMessage = 'Veuillez remplir tous les champs.';
+    }
   }
 }
