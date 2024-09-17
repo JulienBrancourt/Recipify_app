d8fbf3ac76b72d5ee9f803bebe40e7ef47dacee2
LucieRg <129874296+LucieRg@users.noreply.github.com>
2024-09-17 15:29:01 +0200

    favoris + recipe card

--- recipify_front/recipify_front/src/app/pages/favoris/favoris.component.ts
+++ recipify_front/recipify_front/src/app/pages/favoris/favoris.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {Recette} from "../../utils/types/recetteType";
+import {FavorisService} from "../../service/favoris.service";
+import {RecetteCardComponent} from "../../components/recette-card/recette-card.component";
 
 @Component({
   selector: 'app-favoris',
   standalone: true,
-  imports: [],
+  imports: [
+    RecetteCardComponent
+  ],
   templateUrl: './favoris.component.html',
   styleUrl: './favoris.component.css'
 })
 export class FavorisComponent {
+favoris: Recette[] = [];
 
+constructor(private favorisService: FavorisService) {
+  this.favoris = this.favorisService.getFavoris();
+}
+
+removeFromFavorites(title: string) {
+  this.favorisService.removeFavori(title);
+  this.favoris = this.favorisService.getFavoris();
+}
 }
