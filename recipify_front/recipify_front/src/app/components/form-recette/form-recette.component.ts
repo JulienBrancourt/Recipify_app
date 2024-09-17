f4affefb0d87df1523e975dc69f7603b04c3a8de
Fatima-benajjou <fatima.benajjou@gmail.com>
2024-09-17 16:59:22 +0200

    add button ingrdient formRecette

--- recipify_front/recipify_front/src/app/components/form-recette/form-recette.component.ts
+++ recipify_front/recipify_front/src/app/components/form-recette/form-recette.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-form-recette',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './form-recette.component.html',
   styleUrl: './form-recette.component.css'
 })
-export class FormRecetteComponent {
+export class FormRecetteComponent implements OnInit{
 
+  recetteForm: FormGroup;
+
+  constructor(private fb: FormBuilder,private http: HttpClient) {
+    this.recetteForm = this.fb.group({
+
+    })
+  }
+
+
+  addInput() {
+  }
+
+  ngOnInit(): void {
+    this.addInput();
+  }
+
+  onSubmit() {
+
+  }
 }
+
+
