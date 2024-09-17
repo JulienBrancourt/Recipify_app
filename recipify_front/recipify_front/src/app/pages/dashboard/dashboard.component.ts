110a3b4650ebdca329e19b6eca9ce393415f4773
Fatima-benajjou <fatima.benajjou@gmail.com>
2024-09-17 10:46:13 +0200

    formulaire ingrédient

--- recipify_front/recipify_front/src/app/pages/dashboard/dashboard.component.ts
+++ recipify_front/recipify_front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,40 +1,80 @@
-import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class dashboardComponent {
+export class dashboardComponent implements OnInit {
+  ingredientForm: FormGroup;
 
-  constructor(private router: Router) { }
-
-  form = new FormGroup({
-    ingredientName: new FormControl(''),
-    quantity: new FormControl(''),
-  });
-
-  ingredients: string[] = [''];
-
-
-  addInput() {new FormControl ('')
-    
+  constructor(private fb: FormBuilder,private http: HttpClient, private router: Router ) {
+    this.ingredientForm = this.fb.group({
+      ingredients: this.fb.array([]),
+    });
   }
 
-  submitIngredients () {
-    console.log("vos ingrédients sont soumis")
+  ngOnInit(): void {
+    this.addInput(); // Ajoute un champ par défaut au démarrage
   }
 
-    // Fonction pour rediriger vers une autre page
+  get ingredients(): FormArray {
+    return this.ingredientForm.get('ingredients') as FormArray;
+  }
+
+  addInput(): void {
+    const ingredientGroup = this.fb.group({
+      name: [''],
+      quantity: [''],
+      unit: [[]],
+      expiration: ['']
+    });
+    this.ingredients.push(ingredientGroup);
+  }
+
+  unit = [
+    'GRAMMES',
+    'KILOGRAMMES',
+    'LITRES',
+    'CENTILITRES',
+    'MILLILITRES',
+    'CUILLERES_A_SOUPE',
+    'CUILLERES_A_CAFE',
+    'TASSE',
+    'VERRE',
+    'PINCEE',
+    'GOUTTE'
+  ];
+
+  onSubmit(): void {
+    const formIngredientData = this.ingredientForm.value; // Récupération des données du formulaire
+    console.log(formIngredientData);
+    //TODO compléter l'url
+    const url = 'https://example.com/api/APIàcompléter';
+
+    // Envoi de la requête HTTP POST avec les données du formulaire
+    this.http.post(url, formIngredientData).subscribe({
+      next: response =>console.log('Form submitted successfully', response),
+      error: err => (console.error('Error submitting form : ', err))
+      }
+    );
+  }
+
   goToPage() {
     this.router.navigate(['/recette']);
-  }  
-
+  }
 }
+
+
 
